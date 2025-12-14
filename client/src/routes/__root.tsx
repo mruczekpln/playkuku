fe58ae513cadd0c0a6b96798e690e755307e669d
mruczekpln <leonwdev@gmail.com>
2025-12-14 21:59:43 +0100

    chore(client): create the vite project, cleanup

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,21 @@
+import { createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </>
+  ),
+})
